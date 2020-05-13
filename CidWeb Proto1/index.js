8553f1554efeec954be23ea281b1122ea8d1c90f
ePietrocola <enricopietrocola@gmail.com>
2020-05-13 12:48:32 +0200

    hooked up page turning to webrtc

--- CidWeb Proto1/index.js
+++ CidWeb Proto1/index.js
@@ -23,8 +23,9 @@
             peer.on('data', function (data) {
                 data = JSON.parse(data.toString())
                 users[id].update(data)
+                myState.currentPage = data;
                 document.getElementById("current_page")
-                    .value = data;
+                    .value = myState.currentPage;
                 render();
                 console.log("turnPage " + data)
             })
@@ -50,22 +51,56 @@
         })
     }, 100)*/
 
+    document.getElementById('go_previous')
+        .addEventListener('click', (e) => {
+            if(myState.pdf == null
+                || myState.currentPage == 1) return;
+            myState.currentPage -= 1;
+            document.getElementById("current_page")
+                .value = myState.currentPage;
+            render();
+
+            let dataString = JSON.stringify(myState.currentPage)
+            swarm.peers.forEach(function (peer) {
+                peer.send(dataString)
+            })
+        });
+
+    document.getElementById('go_next')
+        .addEventListener('click', (e) => {
+            if(myState.pdf == null
+                || myState.currentPage > myState.pdf
+                    ._pdfInfo.numPages)
+                return;
+
+            myState.currentPage += 1;
+            document.getElementById("current_page")
+                .value = myState.currentPage;
+            render();
+
+            let dataString = JSON.stringify(myState.currentPage)
+            swarm.peers.forEach(function (peer) {
+                peer.send(dataString)
+            })
+
+        });
+
     document.addEventListener('keypress', function (e) {
         switch (e.key) {
             case 'a':
 
-                you.page--
+                /*
                 document.getElementById("current_page")
-                    .value = you.page + 1;
+                    .value = myState.currentPage;
 
                 var youString = JSON.stringify(you.page)
                 swarm.peers.forEach(function (peer) {
                     peer.send(youString)
                 })
-
+                */
                 break
             case 'd':
-
+                /*
                 you.page++
                 document.getElementById("current_page")
                     .value = you.page + 1;
@@ -74,6 +109,8 @@
                 swarm.peers.forEach(function (peer) {
                     peer.send(youString)
                 })
+                */
+
                 break
             case 'w':
                 break
