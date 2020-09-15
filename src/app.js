b7633c17f7cb670b7f05242deb69561ad6fb3591
ePietrocola <enricopietrocola@gmail.com>
2020-09-15 13:26:02 +0200

    re enabled ca

--- src/app.js
+++ src/app.js
@@ -123,7 +123,7 @@ try {
     httpsServer = https.createServer({
       key: fs.readFileSync(key, 'utf8'),
       cert: fs.readFileSync(cert, 'utf8'),
-      //ca: fs.readFileSync(ca, 'utf8')
+      ca: fs.readFileSync(ca, 'utf8')
     }, app).listen(443)
     io = socketio(httpsServer)
     console.log('Https server running')
