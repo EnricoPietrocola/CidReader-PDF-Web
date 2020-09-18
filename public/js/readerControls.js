1039ef040a2baffb2c98266e034de21f8e363d70
ePietrocola <enricopietrocola@gmail.com>
2020-09-18 12:14:18 +0200

    disabled json conversion

--- public/js/readerControls.js
+++ public/js/readerControls.js
@@ -34,10 +34,10 @@ function visualizeDoc(documentLink){
 
     fetch('/get-document?url=' + documentLink + '&roomname=' + roomName)
         .catch(err => console.log(err))
-        .then(res => res.json())
+        //.then(res => res.json())
         .then(res => {
             window.history.replaceState(null, null, "?docURL=" + "\"" + res.url + "\"" );
-            pdfjsLib.getDocument(new Uint8Array(res)).then((pdf) => {
+            pdfjsLib.getDocument(res).then((pdf) => {
                 myState.pdf = pdf;
 
                 myState.currentPage = 1;
