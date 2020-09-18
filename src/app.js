e901cfc379f48616f6ceab1f1109b65dff403bdb
ePietrocola <enricopietrocola@gmail.com>
2020-09-18 12:18:57 +0200

    [test] removed public folder

--- src/app.js
+++ src/app.js
@@ -43,7 +43,7 @@ app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+//app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
   res.render('index', {
