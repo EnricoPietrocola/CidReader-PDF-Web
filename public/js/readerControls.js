fd1416a0a18a2220e3f9c2f008d1d275628451c1
ePietrocola <enricopietrocola@gmail.com>
2020-09-18 17:51:59 +0200

    [debug]

--- public/js/readerControls.js
+++ public/js/readerControls.js
@@ -37,7 +37,7 @@ function visualizeDoc(documentLink){
         //.then(res => res.json())
         .then(res => {
             //window.history.replaceState(null, null, "?docURL=" + "\"" + res.url + "\"" );
-
+            console.log('fetch receiving ' + res)
             /*pdfjsLib.getDocument(res).then((pdf) => {
                 myState.pdf = pdf;
                 myState.currentPage = 1;
