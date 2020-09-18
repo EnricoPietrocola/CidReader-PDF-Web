5d7ebe1f37ada72f6279fece69a3a749a2fd281b
ePietrocola <enricopietrocola@gmail.com>
2020-09-18 18:35:36 +0200

    restore

--- public/js/readerControls.js
+++ public/js/readerControls.js
@@ -34,7 +34,7 @@ function visualizeDoc(documentLink){
 
     fetch('/get-document?url=' + documentLink + '&roomname=' + roomName)
         .catch(err => console.log(err))
-        .then(res => res.json())
+        //.then(res => res.json())
         .then(res => {
             //window.history.replaceState(null, null, "?docURL=" + "\"" + res.url + "\"" );
             console.log('fetch receiving ' + res)
