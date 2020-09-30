103526612c60fd3530b64654bd50ab7f31659372
Enrico Pietrocola <enricopietrocola@gmail.com>
2020-09-30 20:03:57 +0200

    Added keylistener for enter key press on room creation

--- public/js/roomControls.js
+++ public/js/roomControls.js
@@ -3,3 +3,19 @@ function getRoom() {
 
     window.open(roomName,"_self")
 }
+
+document.addEventListener("keydown", function(event) {
+    //if (event.defaultPrevented) {
+    // return; // Do nothing if event already handled
+    //}
+    switch(event.code) {
+        case "Enter":
+            // Handle "left"
+            getRoom()
+            break;
+        default:
+            //default
+            break;
+    }    // Consume the event so it doesn't get handled twice
+    //event.preventDefault();
+}, true);
