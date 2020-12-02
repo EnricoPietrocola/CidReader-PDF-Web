9cbbdc2cc892b20522f2aa69c62dddfd7d5a6235
ePietrocola <enricopietrocola@gmail.com>
2020-12-02 13:08:06 +0100

    fixed fetch call

--- public/pdfjs/web/cidcontrols.js
+++ public/pdfjs/web/cidcontrols.js
@@ -11,7 +11,7 @@ function init(){
   const roomName = window.location.pathname.substring(1)
 
 
-  visualizePublicDoc(document.location.origin + '/welcometocidreader.pdf')
+  visualizePublicDoc(document.location.origin + '/docs/welcometocidreader.pdf')
 
 
   /*fetch('/fetch-document?url=' + document.location.origin + '/docs/welcome.pdf')
