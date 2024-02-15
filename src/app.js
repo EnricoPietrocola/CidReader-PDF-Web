4aac7dbe778ba4390620025ea3dd1226a6d3699d
ePietrocola <enricopietrocola@gmail.com>
2024-02-15 13:28:45 +0100

    Replace hard coded port

--- src/app.js
+++ src/app.js
@@ -209,7 +209,7 @@ app.get('/uploads', (req, res) => {
         key: fs.readFileSync(key, 'utf8'),
         cert: fs.readFileSync(cert, 'utf8'),
         //ca: fs.readFileSync(ca, 'utf8') //hide this if your ssl keys don't include ca
-      }, app).listen(443)
+      }, app).listen(PORT)
       io = socketio(httpsServer)
       console.log('Https server running')
     } else {
